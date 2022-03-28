77d7ceb5b4c9b2ecc615e6f9b4a4329cdb1aee93
mmmmmmmmak <88006224+mmmmmmmmak@users.noreply.github.com>
2022-03-28 21:54:10 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+var app = express();
+var server = app.listen(4000);
+var io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+});
+
+var roomno = 1;
+
+io.on('connection', (socket) => {
+  socket.on('rooms', (no) => {
+    console.log('client wallet is:', no);
+    socket.emit('room', 1);
+  });
+  // client.join("room-"+roomno);
+  // //Send this event to everyone in the room.
+  // io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
+  // client.leave("room-"+roomno);
+});
+
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);

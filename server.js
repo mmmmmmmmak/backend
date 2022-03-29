a48f0fa0a466485566bd47e5c9248cd4a10ddd82
mmmmmmmmak <88006224+mmmmmmmmak@users.noreply.github.com>
2022-03-29 22:24:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,16 @@
-const { response } = require('express');
-var express = require('express');
-var router = express.Router();
-const app = express();
 const { User, createUser, getUserData } = require('./routes/src/panels/mongo.js');
-const bodyParser = require('body-parser');
 const { parse } = require('querystring');
-const crypto = require('crypto');
-const cors = require('cors');
 const url = require('url')
-
-const { useImperativeHandle } = require('react');
-
-/* GET users listing. */
-// app.get('/', (req, res) => {
-
-//   res.send('Successful response.');
-
-// });
-// app.listen(3000, () => console.log('Example app is listening on port 3000.'))
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-//   app.all('/saveUserWallet', async (req, res) => {
-//     let urlRequest = url.parse(req.url, true)
-//     wallett = urlRequest.query.wallet
-//     console.log(urlRequest.query.wallet)
-
-//     let userid = 0
-//     let body = '';
-//     req.on('data', chunk => {
-//       body += chunk.toString();
-//     })
-//     req.on('end', async () => {
-//       const urlParams = parse(body);
-//       userid = urlParams.vk_user_id
-//       res.end('ok')
-//     })
-
-//     const client = await User.find({ user_id: userid });
-//     const data = client[0];
-//     if (!data) {
-
-//           const newUser = new User({
-//               user_id: userid,
-//               wallet: wallett,
-//           });
-  
-//           return newUser.save((error) => {
-  
-//               if (error) {
-//                   return res.statusCode(500).send('Internal Server Error');
-//               }
-  
-//           });
-//         }
-
-// });
-
-var server = app.listen(8000 || proccess.env.PORT);
+const express = require("express")
+var app = express();
+var server = app.listen(8000);
 var io = require('socket.io')(server, {
     cors: {
       origin: '*',
     }
 });
 
+var roomno = 1;
 
 io.on('connection', (socket) => {
   socket.on('mywalletis', (no) => {
@@ -101,8 +48,5 @@ io.on('connection', (socket) => {
 
 
 const port = 8000;
-io.listen(8000);
+io.listen(process.env.PORT || port);
 console.log('listening on port ', port);
-
-
-module.exports = router;

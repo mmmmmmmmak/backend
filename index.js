4aa65f44b916199abab89a2e699311012eaab460
mmmmmmmmak <88006224+mmmmmmmmak@users.noreply.github.com>
2022-03-31 12:56:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,27 @@ io.on('connection', (socket) => {
     console.log('client wallet is:', no);
     socket.emit('wallet', no);
   });
+   socket.on('login', async (wallet, id) => {
+    let find_id = url.parse(id, true)
+    let idisfinds = find_id.query.vk_user_id
+    const client = await User.find({ wallet: wallet });
+    const data = client[0];
+    if (!data) {
+
+          const newUser = new User({
+              user_id: idisfinds,
+              wallet: wallet,
+          });
+  
+          return newUser.save((error) => {
+  
+              if (error) {
+                  return res.statusCode(500).send('Internal Server Error');
+              }
+  
+          });
+        }
+  });
   // client.join("room-"+roomno);
   // //Send this event to everyone in the room.
   // io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
